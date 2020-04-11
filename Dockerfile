6aa5ff0bc7dab501447651a3a23e12ccedae5c5e
enen92 <92enen@gmail.com>
2020-04-11 15:47:41 +0100

    Use git archive to build the ultimate folder destination for addon check

--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,7 @@ FROM python:3.7.5-alpine3.10
 
 RUN apk update && \
     apk --update add libxml2-dev libxslt-dev libffi-dev gcc musl-dev libgcc openssl-dev curl && \
-    apk add jpeg-dev zlib-dev freetype-dev lcms2-dev openjpeg-dev tiff-dev tk-dev tcl-dev
+    apk add jpeg-dev zlib-dev freetype-dev lcms2-dev openjpeg-dev tiff-dev tk-dev tcl-dev git tar
 
 RUN python -m pip install --upgrade pip && \
     pip install kodi-addon-checker
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,2 +1,32 @@
 #!/bin/sh -l
-kodi-addon-checker --branch=$1 --allow-folder-id-mismatch .
+
+# remove dist directory if exists
+if [ -d "dist/addon-checker" ]; then
+  rm -r dist/addon-checker
+fi
+
+# create output directory
+mkdir -p dist/addon-checker
+
+# create the addon zip
+git archive --output=./dist/addon-checker/addon.tar --format=tgz HEAD
+
+# extract the addon zip to the destination where the kodi-addon-checker will run
+if [ -z "$2" ]
+then
+    ADDON_DIR="./dist/addon-checker/addon"
+    FOLDER_ID_MISMATCH="--allow-folder-id-mismatch"
+else
+    ADDON_DIR=./dist/addon-checker/$2
+    FOLDER_ID_MISMATCH=""
+fi
+mkdir -p $ADDON_DIR
+tar zxf ./dist/addon-checker/addon.tar -C $ADDON_DIR
+
+
+if [ "$3" = true ] ; then
+  kodi-addon-checker --branch=$1 $FOLDER_ID_MISMATCH --PR $ADDON_DIR
+else
+  kodi-addon-checker --branch=$1 $FOLDER_ID_MISMATCH $ADDON_DIR
+fi
+
